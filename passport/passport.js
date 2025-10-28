1afc77392b446031b732c787f878f36cd0394b0e
Irene.x <101792723+oneoneirene@users.noreply.github.com>
2025-10-29 01:14:10 +0800

    Update passport.js

--- passport/passport.js
+++ passport/passport.js
@@ -1,6 +1,6 @@
 import passport from 'passport'
-import passportJWT from 'passport-jwt'
 import passportLocal from 'passport-local'
+import passportJWT from 'passport-jwt'
 import bcrypt from 'bcrypt'
 import users from '../models/users.js'
 
@@ -8,24 +8,25 @@ const LocalStrategy = passportLocal.Strategy
 const JWTStrategy = passportJWT.Strategy
 const ExtractJWT = passportJWT.ExtractJwt
 
+// -------------------- Local Strategy (Login) --------------------
 passport.use('login', new LocalStrategy({
   usernameField: 'account',
   passwordField: 'password'
 }, async (account, password, done) => {
   try {
     const user = await users.findOne({ account })
-    if (!user) {
-      return done(null, false, { message: '帳號不存在' })
-    }
-    if (!bcrypt.compareSync(password, user.password)) {
-      return done(null, false, { message: '密碼錯誤' })
-    }
+    if (!user) return done(null, false, { message: '帳號不存在' })
+
+    const isValid = bcrypt.compareSync(password, user.password)
+    if (!isValid) return done(null, false, { message: '密碼錯誤' })
+
     return done(null, user)
-  } catch (error) {
-    return done(error, false)
+  } catch (err) {
+    return done(err, false)
   }
 }))
 
+// -------------------- JWT Strategy --------------------
 passport.use('jwt', new JWTStrategy({
   jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
   secretOrKey: process.env.SECRET,
@@ -36,17 +37,17 @@ passport.use('jwt', new JWTStrategy({
   if (expired && req.originalUrl !== '/users/extend' && req.originalUrl !== '/users/logout') {
     return done(null, false, { message: '登入逾期' })
   }
-  const token = req.headers.authorization.split(' ')[1]
+
+  const token = req.headers.authorization?.split(' ')[1] || ''
   try {
     const user = await users.findById(payload._id)
-    if (!user) {
-      return done(null, false, { message: '使用者不存在' })
-    }
-    if (user.tokens.indexOf(token) === -1) {
-      return done(null, false, { message: '驗證錯誤' })
-    }
+    if (!user) return done(null, false, { message: '使用者不存在' })
+    if (!user.tokens.includes(token)) return done(null, false, { message: '驗證錯誤' })
+
     return done(null, { user, token })
-  } catch (error) {
-    return done(error, false)
+  } catch (err) {
+    return done(err, false)
   }
 }))
+
+export default passport
